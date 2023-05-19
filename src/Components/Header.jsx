04271246fcf21cf48104dec0d1beadf2002edf1b
Smoopfrog <jeffstinson@hotmail.com>
2023-05-18 22:25:52 -0700

    create header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import "../Styles/Header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>A Sweet Romance</h1>
+      <nav>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Something</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

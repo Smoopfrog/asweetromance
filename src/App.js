188c18ca4f811129c37c01579a40732519a0a5ed
Smoopfrog <jeffstinson@hotmail.com>
2023-05-19 00:39:57 -0700

    create locations component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
 import HeroPage from './Components/HeroPage';
+import Locations from './Components/Locations';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroPage />
+      <Locations />
     </div>
   );
 }
---
+++ src/Components/Locations.jsx
@@ -0,0 +1,11 @@
+import "../Styles/Locations.css"
+
+const Locations = () => {
+  return (
+    <section>
+      <h1 className="locations-title">Locations</h1>
+    </section>
+  );
+};
+
+export default Locations

8e66fa1dd12491b2415fff7d037820eb4d2a7e7e
Smoopfrog <jeffstinson@hotmail.com>
2023-05-24 14:50:15 -0700

    style abot button and title

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import "../Styles/About.css";
 const About = () => {
   return (
     <section className="about">
-      <h1>Our Sweet Romance</h1>
+      <h1 className="about-title">Our Sweet Romance</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
         facilis voluptatibus possimus aperiam corporis dicta iste, recusandae

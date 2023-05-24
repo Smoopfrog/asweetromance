cbecd26b7dad580d76867afb896a85f9ac9f59bd
Smoopfrog <jeffstinson@hotmail.com>
2023-05-23 23:56:33 -0700

    fix locations title

--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -4,7 +4,7 @@ import LocationCard from "./LocationCard";
 const Locations = () => {
   return (
     <sectio className="locations-container">
-      <h1 className="locations-title">Locations</h1>
+      <h1 className="locations-title">Our Locations</h1>
       <LocationCard />
     </sectio>
   );

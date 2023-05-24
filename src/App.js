3d1deb531a40ed7243e5f285d8bd595e383421d7
Smoopfrog <jeffstinson@hotmail.com>
2023-05-24 14:34:00 -0700

    create about section and basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Header from './Components/Header';
-import HeroPage from './Components/HeroPage';
-import Locations from './Components/Locations';
+import About from "./Components/About";
+import Header from "./Components/Header";
+import HeroPage from "./Components/HeroPage";
+import Locations from "./Components/Locations";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroPage />
+      <About />
       <Locations />
     </div>
   );
---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+import "../Styles/About.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h1>Our Sweet Romance</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+        facilis voluptatibus possimus aperiam corporis dicta iste, recusandae
+        repellendus dolor ipsum obcaecati ad excepturi quas? Nisi dignissimos
+        doloribus adipisci commodi veniam.
+      </p>
+      <button className="about-btn">Learn More</button>
+    </section>
+  );
+};
+
+export default About;

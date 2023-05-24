d1beba5f968ced76d5b675d3ada1713c98f17bf3
Smoopfrog <jeffstinson@hotmail.com>
2023-05-23 23:50:04 -0700

    add font styling for location card

--- src/Components/LocationCard.jsx
+++ src/Components/LocationCard.jsx
@@ -9,10 +9,18 @@ const LocationCard = () => {
         src={peachland}
         alt="A lovely day showcasing the peachland welcome sign"
       />
-      <h1 className="location-title">Peachland</h1>
-      <div className="location-time">
-        <div className="location-date">Friday</div>
-        <div className="location-hours">9am-2pm</div>
+      <div className="location-info">
+        <h1 className="location-title">
+          Peachland Farmers and Crafters Market
+        </h1>
+        <div className="location-time">
+          <div className="location-date">Friday &nbsp;</div>
+          <div className="location-hours">9am-2pm</div>
+        </div>
+        <div className="location-address">
+          <div>Heritage Park, Beach Ave</div>
+          <div className="location-city">Peachland, BC</div>
+        </div>
       </div>
     </article>
   );

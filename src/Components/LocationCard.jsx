6c5fc02fd091036fcf2754fc94b66bdcb21a54a6
Smoopfrog <jeffstinson@hotmail.com>
2023-05-23 23:33:11 -0700

    add info

--- src/Components/LocationCard.jsx
+++ src/Components/LocationCard.jsx
@@ -1,9 +1,19 @@
 import "../Styles/LocationCard.css";
+import peachland from "../Assets/peachland.webp";
 
 const LocationCard = () => {
   return (
     <article className="location-card">
-      <h1>Hello</h1>
+      <img
+        className="location-img"
+        src={peachland}
+        alt="A lovely day showcasing the peachland welcome sign"
+      />
+      <h1 className="location-title">Peachland</h1>
+      <div className="location-time">
+        <div className="location-date">Friday</div>
+        <div className="location-hours">9am-2pm</div>
+      </div>
     </article>
   );
 };

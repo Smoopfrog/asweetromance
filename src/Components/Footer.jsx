b7bcbffbd44a10c800e2bdf953b0a76dc420a3a9
Smoopfrog <jeffstinson@hotmail.com>
2023-05-25 12:52:28 -0700

    add mail icon to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -34,6 +34,21 @@ const Footer = () => {
           </svg>
           <div className="social-anchor-circle"></div>
         </a>
+        <a
+          className="social-link"
+          href="mailto: asweetromance1@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="social-icon"
+            viewBox="0 0 512 512"
+          >
+            <path d="M101.3 141.6v228.9h0.3 308.4 0.8V141.6H101.3zM375.7 167.8l-119.7 91.5 -119.6-91.5H375.7zM127.6 194.1l64.1 49.1 -64.1 64.1V194.1zM127.8 344.2l84.9-84.9 43.2 33.1 43-32.9 84.7 84.7L127.8 344.2 127.8 344.2zM384.4 307.8l-64.4-64.4 64.4-49.3V307.8z"></path>
+          </svg>
+          <div className="social-anchor-circle"></div>
+        </a>
       </div>
     </footer>
   );

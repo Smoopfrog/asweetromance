90018975dc04bf75bf5cd1c17b404ef132397a6c
Smoopfrog <jeffstinson@hotmail.com>
2023-05-27 23:11:51 -0700

    create html for contact form

---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,14 @@
+const ContactForm = () => {
+  return (
+    <form>
+      <h1>Contact Us!</h1>
+      <input>Full name</input>
+      <input>Email</input>
+      <input>Message</input>
+      <div>email something blah blah blah blah</div>
+      <button>Send</button>
+    </form>
+  );
+};
+
+export default ContactForm;

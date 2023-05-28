b24465527b7bfc7a14e23a41400f61d9da834377
Smoopfrog <jeffstinson@hotmail.com>
2023-05-27 23:17:06 -0700

    fix inputs and add basic styles

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,10 +1,12 @@
+import "../Styles/ContactForm.css";
+
 const ContactForm = () => {
   return (
-    <form>
+    <form className="contact-form">
       <h1>Contact Us!</h1>
-      <input>Full name</input>
-      <input>Email</input>
-      <input>Message</input>
+      <input type="text" placeholder="Name" />
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Message" />
       <div>email something blah blah blah blah</div>
       <button>Send</button>
     </form>

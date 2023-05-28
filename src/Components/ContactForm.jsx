e60cec3525db4e69402853ae558744d3e6cc7557
Smoopfrog <jeffstinson@hotmail.com>
2023-05-27 23:53:15 -0700

    add state to inputs and create dummy email function

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,14 +1,53 @@
+import { useState } from "react";
 import "../Styles/ContactForm.css";
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+    
+    const emailContent = {
+      name,
+      email,
+      message,
+    };
+
+    console.log("emailContent", emailContent);
+  };
+
   return (
     <form className="contact-form">
       <h1 className="contact-title">Contact Us!</h1>
-      <input className="contact-input" type="text" placeholder="Name" />
-      <input className="contact-input" type="email" placeholder="Email" />
-      <textarea className="contact-input contact-input-msg" type="text" placeholder="Message" />
-      <div className="contact-captcha-msg">email something blah blah blah blah</div>
-      <button className="contact-btn">Send</button>
+      <input
+        className="contact-input"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        className="contact-input"
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <textarea
+        className="contact-input contact-input-msg"
+        type="text"
+        placeholder="Message"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <div className="contact-captcha-msg">
+        email something blah blah blah blah
+      </div>
+      <button onClick={sendEmail} className="contact-btn">
+        Send
+      </button>
     </form>
   );
 };

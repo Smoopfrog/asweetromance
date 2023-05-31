a7a9a6673375616cb8dfea5fdfc989443c8ea5f3
Smoopfrog <jeffstinson@hotmail.com>
2023-05-31 13:02:47 -0700

    add styling to errorpage

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -4,8 +4,8 @@ import "../Styles/ErrorPage.css";
 const ErrorPage = () => {
   return (
     <section className="error-page">
-      <h1>404 Page Not Found</h1>
-      <Link to="/">
+      <h1 className="error-page-title">404 Page Not Found</h1>
+      <Link className="error-page-text" to="/">
         The page you requested does not exist. Click here to continue shopping.
       </Link>
     </section>

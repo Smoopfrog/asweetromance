a80d9b954fd141f5989560ac7c627d85c479b7dd
Smoopfrog <jeffstinson@hotmail.com>
2023-06-01 23:04:32 -0700

    fix about page img

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -4,7 +4,7 @@ import couplePic from "../../Assets/cuties.jpg";
 const AboutPage = () => {
   return (
     <section className="about-page">
-      <div className="about-page-banner">Our Sweet Romance</div>
+      {/* <div className="about-page-banner">Our Sweet Romance</div> */}
       <article className="about-page-article">
         <img
           className="about-page-img"
@@ -17,22 +17,7 @@ const AboutPage = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
             veniam nam reiciendis architecto dolorem voluptatem quo sint facere
             natus voluptates esse cum, dignissimos quidem enim omnis adipisci
-            nemo cupiditate quibusdam.
-          </p>
-        </div>
-      </article>
-      <article className="about-page-article">
-        <img
-          className="about-page-img"
-          src={couplePic}
-          alt="a bunch of cuties"
-        />
-        <div className="about-page-text">
-          <h2 className="about-page-text-title">Something Something</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-            veniam nam reiciendis architecto dolorem voluptatem quo sint facere
-            natus voluptates esse cum, dignissimos quidem enim omnis adipisci
+            nemo cupiditate quibusdam. Dignissimos quidem enim omnis adipisci
             nemo cupiditate quibusdam.
           </p>
         </div>

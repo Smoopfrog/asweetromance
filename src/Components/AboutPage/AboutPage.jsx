383c95c895eedf955fff2a55620d781adeba1dce
Smoopfrog <jeffstinson@hotmail.com>
2023-06-07 23:54:33 -0700

    add viewport styling for about page

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -5,24 +5,17 @@ import { Link } from "react-router-dom";
 const AboutPage = () => {
   return (
     <section className="about-page">
-      {/* <div className="about-page-banner">Our Sweet Romance</div> */}
-      <article className="about-page-article">
-        <img
-          className="about-page-img"
-          src={couplePic}
-          alt="a bunch of cuties"
-        />
-        <div className="about-page-text">
-          <h2 className="about-page-text-title">Our Sweet Romance</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-            veniam nam reiciendis architecto dolorem voluptatem quo sint facere
-            natus voluptates esse cum, dignissimos quidem enim omnis adipisci
-            nemo cupiditate quibusdam. Dignissimos quidem enim omnis adipisci
-            nemo cupiditate quibusdam.
-          </p>
-        </div>
-      </article>
+      <img className="about-page-img" src={couplePic} alt="a bunch of cuties" />
+      <div className="about-page-text">
+        <h2 className="about-page-text-title">Our Sweet Romance</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          veniam nam reiciendis architecto dolorem voluptatem quo sint facere
+          natus voluptates esse cum, dignissimos quidem enim omnis adipisci nemo
+          cupiditate quibusdam. Dignissimos quidem enim omnis adipisci nemo
+          cupiditate quibusdam.
+        </p>
+      </div>
       <Link className="about-page-btn" to="/Contact">
         Contact Us
       </Link>

fb8000bd7fa23af0da8aede8358792cc12b7d94f
Smoopfrog <jeffstinson@hotmail.com>
2023-06-09 23:46:19 -0700

    add styling to admin-login

--- src/Components/AdminPage.jsx
+++ src/Components/AdminPage.jsx
@@ -1,12 +1,14 @@
-import "../Styles/AdminPage.css"
+import "../Styles/AdminPage.css";
 
 const AdminPage = () => {
   return (
     <section className="admin-page">
-      <h1>Admin Login</h1>
-      <input placeholder="Username" />
-      <input placeholder="password" type="password" />
-      <button>Log In</button>
+      <form className="admin-login-form">
+        <h1 className="admin-login-title">Admin Login</h1>
+        <input className="admin-input" placeholder="Username" />
+        <input className="admin-input" placeholder="password" type="password" />
+        <button className="admin-page-btn">Log In</button>
+      </form>
     </section>
   );
 };
